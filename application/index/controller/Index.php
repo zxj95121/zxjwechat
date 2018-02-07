31c32ae8cb7d6e799cd6c637fc5cac39671918c6
zxj95121 <986773071@qq.com>
2018-02-07 10:26:53 +0800

    no message

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,6 +3,7 @@ namespace app\index\controller;
 
 use think\Session;
 use think\Weixin;
+use think\Request;
 
 use app\common\controller\FrontBase;
 class Index extends FrontBase
@@ -17,11 +18,26 @@ class Index extends FrontBase
     }
 
     /* 展示JSSDK用例 */
-    public function index(){
+    public function index()
+    {
         $weixin = new Weixin($this->account_id);
         $config = $weixin->getJsApiTicket();
         
         $this->assign('config', $config);
         return $this->fetch('/index');
+    }
+
+    /* 调用百度API获取地理位置 */
+    public function getPosition()
+    {
+        $request = Request::instance()->param();
+        $lat = $request['lat'];
+        $long = $request['long'];
+        $url = 'http://api.map.baidu.com/geocoder?location='.$lat.','.$long.'&output=json&key=aibHOrzNpo585L0P3HIa3lGK';
+
+        // $weixin = new Weixin($this->account_id);
+        // $weixin->curlPost($url);
+        $result = file_get_contents($url);
+        echo $result;
     }
 }
